0afa1373790bec912ec633deb2debf20e166eba0
Emmanuel Olaguer <2olage06@solent.ac.uk>
2018-03-14 10:52:13 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2olage06/android5uis/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.a2olage06.android5uis;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView sv = (SearchView) MenuItemCompat.getActionView(item);
+        sv.setOnQueryTextListener(new SearchHandler());
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.save)
+        {
+            new AlertDialog.Builder(this).setPositiveButton("OK", null).
+                    setMessage("Saving").show();
+            return true;
+        }
+        return false;
+    }
+
+    class SearchHandler implements SearchView.OnQueryTextListener {
+
+
+        public SearchHandler(){
+
+        }
+
+        public boolean onQueryTextChange(String txt) {
+            // do nothing... (this method runs when the user types a new character)
+            return true;
+        }
+
+        public boolean onQueryTextSubmit(String txt) {
+            // show the search text in an alert dialog
+            new AlertDialog.Builder(MainActivity.this).setPositiveButton("OK", null).
+                    setMessage(txt).show();
+            return true;
+        }
+    }
+
+
+}
+
